67c725a441ea9a8fc377e6defb76ab56c3eb46a7
Junaid Qureshi <Junaid.562000@gmail.com>
2022-01-05 09:33:12 +0530

    5/1/22

---
+++ 10.JS/learning/app.js
@@ -0,0 +1,15 @@
+// document.getElementById('title').innerText = 'This is written by JS';
+
+let a = 10;
+document.write(`a = ${10}`);
+
+document.write('--------------------' + '<br>');
+
+// Loops
+for (let i = 0; i < 10; i++) {
+  document.write(i + '<br>');
+}
+
+while (a-- > 0) {
+  document.write(a + '<br>');
+}
